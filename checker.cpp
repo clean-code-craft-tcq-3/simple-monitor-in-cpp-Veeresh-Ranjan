0540a5815941b7cd5c61db06bfadbb8bbdd60623
Veeresh-Ranjan <99407299+Veeresh-Ranjan@users.noreply.github.com>
2022-05-11 02:40:29 +0530

    Update checker.cpp

--- checker.cpp
+++ checker.cpp
@@ -5,11 +5,11 @@ int main() {
   checkerObj.checkTemperature();
   checkerObj.checkSoc();
   checkerObj.checkChargeRate();
-  checkerObj.batteryIsOk();
+  //checkerObj.batteryIsOk();
   checker checkerObj2(new BatteryMgmtSystmTest(50, 85, 0));
   checkerObj2.checkTemperature();
   checkerObj2.checkSoc();
   checkerObj2.checkChargeRate();
-  checkerObj2.batteryIsOk();
+  //checkerObj2.batteryIsOk();
 }
 
